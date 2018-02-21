9f1098530466c05505febbfcc29d531e3ac972a7
Paul Woitaschek <woitaschek@posteo.de>
2018-02-21 15:20:24 +0100

    Update MultipleJsonQualifiers.kt

--- tests/src/main/kotlin/se/ansman/kotshi/MultipleJsonQualifiers.kt
+++ tests/src/main/kotlin/se/ansman/kotshi/MultipleJsonQualifiers.kt
@@ -3,9 +3,11 @@ package se.ansman.kotshi
 import com.squareup.moshi.JsonQualifier
 
 @JsonQualifier
+@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 annotation class WrappedInObject
 
 @JsonQualifier
+@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 annotation class WrappedInArray
 
 @JsonSerializable
